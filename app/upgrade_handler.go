81480a66a5288d97af3d4e6d7624a804c9918b39
Marina Gonokhina <marina.gonokhina@fulldive.com>
2022-02-08 12:52:24 +0300

    uncomment and fix v2.3 upgrade handler

--- app/upgrade_handler.go
+++ app/upgrade_handler.go
@@ -11,7 +11,7 @@ import (
 
 func (app ImversedApp) setUpgradeHandler(cfg module.Configurator) {
 	app.UpgradeKeeper.SetUpgradeHandler(
-		"2.1",
+		"v2.1",
 		func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 			err := cfg.RegisterMigration("nft", 2, app.NFTKeeper.MigrationAddOracleUrl)
 			if err != nil {
@@ -27,7 +27,7 @@ func (app ImversedApp) setUpgradeHandler(cfg module.Configurator) {
 		panic(fmt.Sprintf("failed to read upgrade info from disk %s", err))
 	}
 
-	if upgradeInfo.Name == "2.2" && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+	if upgradeInfo.Name == "v2.2" && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		storeUpgrades := storetypes.StoreUpgrades{
 			Added: []string{currencymoduletypes.ModuleName},
 		}
@@ -36,15 +36,15 @@ func (app ImversedApp) setUpgradeHandler(cfg module.Configurator) {
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
 	}
 
-	//app.UpgradeKeeper.SetUpgradeHandler(
-	//	"2.3",
-	//	func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-	//		// does nothing, is necessary only because ob nft version was changed
-	//		err := cfg.RegisterMigration("nft", 3, func(ctx sdk.Context) error { return nil })
-	//		if err != nil {
-	//			return nil, err
-	//		}
-	//		return app.mm.RunMigrations(ctx, cfg, vm)
-	//	},
-	//)
+	app.UpgradeKeeper.SetUpgradeHandler(
+		"v2.3",
+		func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+			// does nothing, is necessary only because ob nft version was changed
+			err := cfg.RegisterMigration("nft", 3, func(ctx sdk.Context) error { return nil })
+			if err != nil {
+				return nil, err
+			}
+			return app.mm.RunMigrations(ctx, cfg, vm)
+		},
+	)
 }
