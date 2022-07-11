5b08a0ead943577b2e978a1879fa0b6f1a6fb2b8
MagHermit <vladkakurin007@gmail.com>
2022-07-11 17:16:16 +1000

    add cli functions

---
+++ x/verse/client/cli/query.go
@@ -0,0 +1,126 @@
+package cli
+
+import (
+	"context"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/imversed/imversed/x/verse/types"
+	"github.com/spf13/cobra"
+)
+
+// GetQueryCmd returns the parent command for all verse CLI query commands.
+func GetQueryCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      "Querying commands for the erc20 module",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		GetVersesCmd(),
+		GetVerseCmd(),
+		GetParamsCmd(),
+	)
+	return cmd
+}
+
+// GetVersesCmd queries a token pairs registered
+func GetVersesCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "verses",
+		Short: "Gets all verses registered",
+		Long:  "Gets all verses registered",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			req := &types.QueryAllVerseRequest{
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.VerseAll(context.Background(), req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetVerseCmd queries a token pairs registered
+func GetVerseCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "verse [name]",
+		Short: "Get a verse registered",
+		Long:  "Get a verse registered",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			req := &types.QueryGetVerseRequest{
+				Name: args[0],
+			}
+
+			res, err := queryClient.Verse(context.Background(), req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetHubParamsCmd queries hub info
+func GetParamsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Short: "Gets verse module params",
+		Long:  "Gets verse module params",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			req := &types.QueryParamsRequest{}
+
+			res, err := queryClient.Params(context.Background(), req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
