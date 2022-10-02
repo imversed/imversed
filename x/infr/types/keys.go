73fdbcf146f89c340df8bfe4ffa672c80b97bc64
MagHermit <vladkakurin007@gmail.com>
2022-10-03 00:06:35 +0400

    add smart-contracts metadata creation and refactor infr module

--- x/infr/types/keys.go
+++ x/infr/types/keys.go
@@ -13,19 +13,22 @@ const (
 
 	// RouterKey is the message route for slashing
 	RouterKey = ModuleName
+
+	// SmartContractKeyPrefix is the prefix to retrieve all Currency
+	SmartContractKeyPrefix = "Smart_contract/address/"
 )
 
 func KeyPrefix(p string) []byte {
 	return []byte(p)
 }
 
-// InfrKey returns the store key to retrieve a Currency from the index fields
-func InfrKey(
-	denom string,
+// InfrSmartContractKey returns the store key to retrieve a smart-contract from the index fields
+func InfrSmartContractKey(
+	address string,
 ) []byte {
 	var key []byte
 
-	denomBytes := []byte(denom)
+	denomBytes := []byte(address)
 	key = append(key, denomBytes...)
 	key = append(key, []byte("/")...)
 
