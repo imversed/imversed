9ab4be89c00547c08e28b179c18bd5aed6d3cd9c
MagHermit <vladkakurin007@gmail.com>
2022-10-10 03:20:45 +0700

    update query functional

--- x/verse/keeper/grpc_query.go
+++ x/verse/keeper/grpc_query.go
@@ -2,10 +2,12 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/imversed/imversed/x/verse/types"
+	"golang.org/x/exp/slices"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -20,10 +22,10 @@ func (k Keeper) Verse(c context.Context, req *types.QueryGetVerseRequest) (*type
 
 	val, found := k.GetVerse(
 		ctx,
-		req.Name,
+		req.VerseName,
 	)
 	if !found {
-		return nil, status.Error(codes.InvalidArgument, "not found")
+		return nil, status.Error(codes.InvalidArgument, " verse not found")
 	}
 
 	return &types.QueryGetVerseResponse{Verse: val}, nil
@@ -64,4 +66,45 @@ func (k Keeper) Params(c context.Context, req *types.QueryParamsRequest) (*types
 	ctx := sdk.UnwrapSDKContext(c)
 
 	return &types.QueryParamsResponse{Params: k.GetParams(ctx)}, nil
+}
+
+func (k Keeper) HasAsset(c context.Context, req *types.QueryHasAssetRequest) (*types.QueryHasAssetResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(c)
+
+	val, found := k.GetVerse(
+		ctx,
+		req.VerseName,
+	)
+	if !found {
+		return nil, status.Error(codes.InvalidArgument, "verse not found")
+	}
+
+	switch req.AssetType {
+	case types.ContractType:
+		return &types.QueryHasAssetResponse{HasAsset: slices.Contains(val.SmartContracts, req.AssetId)}, nil
+	default:
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("invalid assert type: %s", req.AssetType))
+	}
+}
+
+func (k Keeper) GetAssets(c context.Context, req *types.QueryGetVerseAssetsRequest) (*types.QueryGetVerseAssetsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(c)
+
+	val, found := k.GetVerse(
+		ctx,
+		req.VerseName,
+	)
+	if !found {
+		return nil, status.Error(codes.InvalidArgument, "verse not found")
+	}
+
+	return &types.QueryGetVerseAssetsResponse{
+		Assets: val.SmartContracts,
+	}, nil
 }
