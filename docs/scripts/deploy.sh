a412ec9e3458b23c1819e15b32e57869286ae7b4
denise <denis.esaulov@imversed.com>
2022-10-17 10:29:46 +0200

    move script to docs folder

--- docs/scripts/deploy.sh
+++ docs/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
-gsutil -m rsync -R ./docs/src/.vuepress/dist gs://docs.imversed.com
+gsutil -m rsync -R ./src/.vuepress/dist gs://docs.imversed.com
 #https://cloud.google.com/storage/docs/gsutil/commands/web
 gsutil web set -m index.html -e index.html gs://docs.imversed.com
