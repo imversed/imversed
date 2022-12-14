88369dc145eca1d28d71f03afb9b10ced70cb5a4
MagHermit <vladkakurin007@gmail.com>
2022-12-15 00:09:17 +0700

    fix account type while updating

--- app/upgrade_handler.go
+++ app/upgrade_handler.go
@@ -3,7 +3,9 @@ package app
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	infrtypes "github.com/imversed/imversed/x/infr/types"
 )
 
 func (app ImversedApp) setUpgradeHandler(cfg module.Configurator) {
@@ -66,6 +68,19 @@ func (app ImversedApp) setUpgradeHandler(cfg module.Configurator) {
 	app.UpgradeKeeper.SetUpgradeHandler(
 		"v3.11",
 		func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+			return app.mm.RunMigrations(ctx, cfg, vm)
+		},
+	)
+	app.UpgradeKeeper.SetUpgradeHandler(
+		"v3.12",
+		func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+
+			// re-create infr module account
+			_, perms := app.AccountKeeper.GetModuleAddressAndPermissions(infrtypes.ModuleName)
+			macc := types.NewEmptyModuleAccount(infrtypes.ModuleName, perms...)
+			maccI := (app.AccountKeeper.NewAccount(ctx, macc)).(types.ModuleAccountI) // set the account number
+			app.AccountKeeper.SetModuleAccount(ctx, maccI)
+
 			return app.mm.RunMigrations(ctx, cfg, vm)
 		},
 	)
