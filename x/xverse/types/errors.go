8af8b894e64525e5261dd53366d1a54fb99def1c
MagHermit <vladkakurin007@gmail.com>
2023-01-12 02:37:59 +0700

    register new error codes

--- x/xverse/types/errors.go
+++ x/xverse/types/errors.go
@@ -3,10 +3,12 @@ package types
 // DONTCOVER
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	sdkerrors "cosmossdk.io/errors"
 )
 
 // x/verse module sentinel errors
 var (
-	ErrVerseAlreadyExists = sdkerrors.Register(ModuleName, 3, "verse already exists")
+	ErrVerseAlreadyExists    = sdkerrors.Register(ModuleName, 3, "verse already exists")
+	ErrContractAlreadyMapped = sdkerrors.Register(ModuleName, 4, "contract already mapped")
+	ErrContractNotFound      = sdkerrors.Register(ModuleName, 5, "contract not found")
 )
