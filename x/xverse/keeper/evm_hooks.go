bf1fd78db63e7a416f6267839a2403eb00c56b67
MagHermit <vladkakurin007@gmail.com>
2023-01-21 02:52:24 +0700

    create hook for permissions control in verse's contract usage

--- x/xverse/keeper/evm_hooks.go
+++ x/xverse/keeper/evm_hooks.go
@@ -1,10 +1,13 @@
 package keeper
 
 import (
+	sdkerrors "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/core"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
+	"github.com/imversed/imversed/x/xverse/types"
+	"golang.org/x/exp/slices"
 )
 
 type Hooks struct {
@@ -23,6 +26,31 @@ func (h Hooks) PostTxProcessing(
 	msg core.Message,
 	receipt *ethtypes.Receipt,
 ) error {
+	// if smart-contract deploying
+	if msg.To() == nil {
+		return nil
+	}
+
+	contract, found := h.k.GetContract(ctx, msg.To().Hex())
+	if !found {
+		return nil
+	}
+
+	verse, found := h.k.GetVerse(ctx, contract.Verse)
+
+	if !found {
+		return nil
+	}
+
+	cosmosAddress := sdk.AccAddress(msg.From().Bytes())
+
+	if verse.Owner == cosmosAddress.String() {
+		return nil
+	}
+
+	if !slices.Contains(verse.AuthenticatedKeys, cosmosAddress.String()) {
+		return sdkerrors.Wrapf(types.ErrNotAuthenticated, "%s", cosmosAddress.String())
+	}
 
 	return nil
 }
--- x/xverse/types/errors.go
+++ x/xverse/types/errors.go
@@ -11,4 +11,5 @@ var (
 	ErrVerseAlreadyExists    = sdkerrors.Register(ModuleName, 3, "verse already exists")
 	ErrContractAlreadyMapped = sdkerrors.Register(ModuleName, 4, "contract already mapped")
 	ErrContractNotFound      = sdkerrors.Register(ModuleName, 5, "contract not found")
+	ErrNotAuthenticated      = sdkerrors.Register(ModuleName, 6, "key not authorized")
 )
