c6ec54275ba8859f31424f8f8abbb6334aafba8f
MagHermit <vladkakurin007@gmail.com>
2023-01-31 12:06:01 +0400

    add base for svc denom

--- x/currency/types/msgs.go
+++ x/currency/types/msgs.go
@@ -82,3 +82,39 @@ func (msg *MsgIssue) ValidateBasic() error {
 	}
 	return nil
 }
+
+func NewMsgBurn(sender string, coin sdk.Coin) *MsgBurn {
+	return &MsgBurn{
+		Sender: sender,
+		Coin:   coin,
+	}
+}
+
+func (msg *MsgBurn) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgBurn) Type() string {
+	return "Burn"
+}
+
+func (msg *MsgBurn) GetSigners() []sdk.AccAddress {
+	sender, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{sender}
+}
+
+func (msg *MsgBurn) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgBurn) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid sender address (%s)", err)
+	}
+	return nil
+}
